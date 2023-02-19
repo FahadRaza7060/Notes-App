38a9eead225b397a999c06c2d8a68bf6a87facf5
FahadRaza7060 <fahadraza7060@gmail.com>
2023-02-19 17:24:23 +0500

    login successfully

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,50 +1,71 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "../slices/authSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function Login() {
-
-  const[data,setdata ] = useState(null); 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [data, setdata] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth);
+
+  console.log({ userData });
 
   useEffect(() => {
-    if(data){
-      // console.log("data",data)
-         let path="/";
-          navigate(path);
+    if (userData.user?.authtoken) {
+      let path = "/";
+      navigate(path);
     }
-      }, [data])
+  }, [userData]);
 
-      const onSubmit = async(e)=>{
-        e.preventDefault();
-        const y = await dispatch(login({email,password}));
-        // console.log("y.payload",y.payload)
-        const token = y.payload.token
-        // console.log("token",token);
-        setdata(token);
-        setEmail("")
-        setPassword("" );
-      }      
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const y = dispatch(login({ email, password }));
+    // console.log("y.payload",y.payload)
+    // const token = y.payload.token;
+    console.log("token: ", y);
+    // setdata(token);
+    setEmail("");
+    setPassword("");
+  };
 
   return (
     <>
-    <h1 className="text-center my-3">Login</h1>
+      <h1 className="text-center my-3">Login</h1>
       <div className="container my-3">
-      <form onSubmit={onSubmit}>
-      <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input type="password" className="form-control" id="password" name="password"  value={password} onChange={(e)=>setPassword(e.target.value)} />
-        </div>
-        <button type="submit" className="btn btn-primary" >Login</button>
-      </form>
+        <form onSubmit={onSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+        </form>
       </div>
     </>
   );
